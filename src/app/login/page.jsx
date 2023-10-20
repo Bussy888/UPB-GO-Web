6f973efb7a5b6b3d452c82945242b254fcc7efff
Vivian Marino <marino6733784@gmail.com>
2023-10-19 22:19:09 -0400

    login inicial

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -14,18 +14,34 @@ const LoginPage = () =>{
 
     return(
         <>
-        <div className='w-full h-screen bg-[#F2F2F2]'>
-            <div className='flex flex-col bg-[#EAEAEA] border-2 border-black'>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <label className=" text-2xl">Email</label>
+        <div className='flex w-full h-screen bg-[#F2F2F2] justify-center align-middle items-center'>
+            <div className='flex flex-col bg-[#EAEAEA] border-2 border-black py-6 px-6'>
+                <form className='flex flex-col gap-10' onSubmit={handleSubmit(onSubmit)}>
+                    <div className='flex flex-col'>
+                        <label className=" text-xl text-black mb-5 font-medium">Email</label>
                         <input
                             type="text"
-                            className=" text-base border-2 border-black w-72 h-9 invalid:border-red-800"
+                            className=" text-base border-2 border-black w-72 h-9 invalid:border-red-800 px-4 py-3 text-black"
                             placeholder="ejemplo123@upb.com"
                             {...register('email', {
                                 required: true
-                            })}/>
+                            })}
+                        />
                         {errors.email?.type === 'required' && <h1 className=" text-base text-red-700">*Debe llenar este campo</h1>}
+                    </div>
+                    <div className='flex flex-col'>
+                        <label className=" text-xl text-black mb-5 font-medium">Password</label>
+                        <input
+                            type="text"
+                            className=" text-base border-2 border-black w-72 h-9 invalid:border-red-800 px-4 py-3 text-black"
+                            placeholder="*******"
+                            {...register('password', {
+                                required: true
+                            })}
+                        />
+                        {errors.password?.type === 'required' && <h1 className=" text-base text-red-700">*Debe llenar este campo</h1>}
+                    </div>
+                    <button className=' flex text-xl font-medium w-76 h-9 bg-[#929292] px-5 py-6 text-black justify-center items-center align-middle' type="submit">Continuar</button>
 
                 </form>
                 
