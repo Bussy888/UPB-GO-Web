d3aa6246ab548673c137ed2804722ab113e3dfdb
Vivian Marino <marino6733784@gmail.com>
2023-10-22 21:50:28 -0400

    merge conflicts

--- src/firebase/initializeFirebase.js
+++
@@ -1,9 +0,0 @@
-import Firebase from 'firebase/app';
-import 'firebase/auth';
-import FirebaseCredentials from './config';
-
-if(!Firebase.getApps.length){
-    Firebase.initializeApp(FirebaseCredentials);
-}
-
-export default Firebase;
